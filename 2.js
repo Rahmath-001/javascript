056dd42fa85f3aff50e7b4a248f76c48a7170a93
Rahmath-001 <rehamathullah7@gmail.com>
2022-07-18 10:48:10 +0530

    this is javascript practice program file.

---
+++ 2.js
@@ -0,0 +1,10 @@
+var e;
+d=8;
+a=12
+
+b
+=
+13
+
+c  =            14
+console.log(a,b,c,d,e)
---
+++ 3.js
@@ -0,0 +1,12 @@
+// console.log(1+2)
+// console.log("abc")
+// var a,b;
+// a,b=2,4
+// console.log(b,a)
+var a
+a=a+4;
+console.log(typeof a,a)
+
+
+
+
