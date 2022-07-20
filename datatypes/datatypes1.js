a1618e5ff0f268785fa4759fc7fda51800712165
Rahmath-001 <rehamathullah7@gmail.com>
2022-07-20 12:47:57 +0530

    this is updated files of javascript practice.

--- datatypes/datatypes1.js
+++ datatypes/datatypes1.js
@@ -8,81 +8,118 @@
 
 
 // var a = 0b0110
-
 // console.log(a.toString(10))
+// console.log(a.toString(8))
+// console.log(a.toString(16))
 
+// var a = 110
+// console.log(a.toString(10))
+// console.log(a.toString(8))
+// console.log(a.toString(16))
 
 
+// var a = 0o110
+// console.log(a.toString(10))
+// console.log(a.toString(2))
+// console.log(a.toString(16))
 
 
+// var a = 0x110
+// console.log(a.toString(10))
+// console.log(a.toString(8))
+// console.log(a.toString(2))
 
 
 
+//  URINARY OPERATOR
 
+// console.log(1)
+// console.log(+1)
+// console.log(-1)
 
+// console.log(1)
+// console.log(+'1')
+// console.log(-'1')
 
 
 
+// Console.log(typeof NaN)
 
+// var a=1, b='1', c=true, d='true';
 
+// console.log(a,typeof a)
+// console.log(+a,typeof +a)
+// console.log(-a,typeof -a)
 
+// console.log(b,typeof b)
+// console.log(+b,typeof +b)
+// console.log(-b,typeof -b)
 
+// console.log(c,typeof c)
+// console.log(+c,typeof +c)
+// console.log(-c,typeof -c)
 
+// console.log(d,typeof d)
+// console.log(+d,typeof +d)
+// console.log(-d,typeof -d)
 
 
 
+// console.log(false + false) // 0
+// console.log(false + true) // 1
+// console.log(false + 0) // 0
+// console.log(true + 0) // 1
+// console.log(1 + true) // 2
 
 
 
+// console.log(false + 'false')
+// console.log(false + 'true')
+// console.log(false + '0') 
+// console.log(true + '0') 
+// console.log(1 + 'true')
 
 
+// console.log(false - 'false')
+// console.log(false - 'true')
+// console.log(false - '0') 
+// console.log(true - '0') 
+// console.log(1 - 'true')
 
 
+// <, >, are used to give true, false.
+// console.log(true == "true") // true is boolean, "true" is string.
+// console.log(true >= "false")
+// console.log(true <= "true")
+// console.log(true == "false")
+// console.log(true <= "false")
+// console.log(false >= "true")
+// console.log(false <= "false")
+// console.log(false == "true")
+// console.log(false == "false")
+// console.log(false < "true")
+// console.log(true > "true")
+// console.log(true < "true")
 
 
 
 
 
 
+// console.log('1' + '2')
+// console.log('1' + 2)
+// console.log(1 + '2')
+// console.log(100 + 1 + '2')
+// console.log('100' + 1 + '2')
+// console.log('100' + 1 + 2)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log('1' - '2')
-console.log('1' - 2)
-console.log(1 - '2')
-console.log(100 - 1 + '2')
-console.log('100' - 1 - '2')
-console.log('100' - 1 - 2)
+// console.log('1' - '2')
+// console.log('1' - 2)
+// console.log(1 - '2')
+// console.log(100 - 1 + '2')
+// console.log('100' - 1 - '2')
+// console.log('100' - 1 - 2)
 
