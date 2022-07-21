49cb00dad3b2926a5bc943a6b5b1f7a2cd24fe62
Rahmath-001 <rehamathullah7@gmail.com>
2022-07-21 10:01:10 +0530

    This is the updated files of javascript practice.

---
+++ datatypes/datatypes-2.js
@@ -0,0 +1,40 @@
+// console.log(4 / "CFI");
+// console.log(89 - "CFI");
+
+
+// console.log(1 / 0);
+// console.log(1 / Infinity);
+// console.log(Infinity / Infinity);
+// console.log(-Infinity / 0);
+// console.log(Infinity / 0);
+
+
+// console.log(NaN == NaN)
+// console.log(NaN === NaN)
+ 
+
+// var value = NaN;
+// console.log(typeof value);
+
+
+// var value = Infinity;
+// console.log(typeof value);
+
+
+
+
+
+
+// TERNARY OPERATOR (? :)
+// condition   ?  true statement : false statement
+
+// var a=16
+// a>12  ? console.log('yes') : console.log('no')
+
+// kid(<12), teen(<18), adult(>18)
+
+// a<18 ? a<12 ? console.log('kid') : console.log('teen') : console.log('adult')
+
+
+
+
---
+++ datatypes/datatypes-3.js
@@ -0,0 +1,75 @@
+// PRIMITIVE DATATYPES
+// NUMBER
+// STRING
+// BOOLEAN
+
+// COMPOSITE DATATYPES
+// ARRAY
+// OBJECT
+// FUNCTION
+
+
+// UNDEFINIED
+
+
+// Comparision of var, let, const.
+// Hoisting
+
+
+
+// STRING 
+// Its denoted in    ""   or    ''
+
+// var a = 'i am string'
+// var a = 'iamstring'
+// var a = "i am string"
+// var a = new String('i am also a string')
+// console.log(a, '\n', typeof a)
+
+// var a= "hi i'am a 'joker'"
+// var a= "hi i\'am a \"joker\""   //ESCAPE CHARACTER - \
+// console.log(a, '\n', typeof a)
+
+
+
+// var a = "Najeeb"
+// var b = 'smart'
+
+// console.log(a + ' is not ' + b + ' because he is sleepy')
+// console.log(a, 'is not', b, 'because he is sleepy')
+// console.log(`${a} is not ${b} because he is sleepy`)  // ' ` ' SYmbol used to use ' $ ' for strings.
+
+
+
+// var a = 'smart'
+// INDEXING
+// every index has their number.
+// indexing starts with zero.
+// even whitespace is a character.
+// if starting from right we will have negative numbers from -1.
+// we use square brackets to access the index values.
+// SYNTAX :      a.slice(number of index)
+// console.log(a[3])
+// console.log(a.slice(1):(4))  //end value always get excluded.
+// console.log(a.slice(-1,4))  // slice runs only from left to right.
+// console.log(a.slice(-1,-4)) 
+// console.log(a.slice(-3,4))    //  * * *
+
+
+var a = 'smart'
+console.log(a.indexOf('s')) 
+console.log(a.indexOf('m')) 
+console.log(a.indexOf('a')) 
+console.log(a.indexOf('r')) 
+console.log(a.indexOf('t')) 
+console.log(a[-3])
+console.log(a.indexOf(3)) 
+
+console.log(a.indexOf(m)) 
+
+
+
+
+
+
+
