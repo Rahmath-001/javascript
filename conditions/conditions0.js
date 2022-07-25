f1ddce41d16fa65797362ebb1578cea10a6ae407
Rahmath-001 <rehamathullah7@gmail.com>
2022-07-25 10:46:54 +0530

    pacticing conditions and flow

---
+++ conditions/conditions0.js
@@ -0,0 +1,109 @@
+// CONTROL FLOW
+
+// 1. SEQUENTIAL 
+// 2. CONDITIONS 
+// 3. ITTERATIONS
+
+
+
+
+
+
+
+
+
+// 1. if condition
+
+// if (condition) {
+    // statement
+// }
+
+// if (1) { console.log('i am one')}
+
+// if (true) {console.log('i am one') }
+
+// if (hey) {console.log('i am one') }
+
+// if ('0') {console.log('i am one') }
+
+// if ('4') {console.log('i am one') }
+
+// if (0) {console.log('i am one') }
+
+// if (null) {console.log('i am one') }
+
+// if ([]) {console.log('i am one') }
+
+// if ('') {console.log('i am one') }
+
+// if ('0.0') {console.log('i am one') }
+
+// if (0.0) {console.log('i am one') }
+
+
+// if (3>4) {
+//     console.log('cat1')
+// }
+
+
+
+// if ((0) && []) {
+//     console.log('cat1')
+// }
+
+// if (8&7) {
+//     console.log('cat2')
+// }
+
+
+
+// if ([1,2]==[1,2]) {
+//     console.log('cat1')
+// }
+
+// if ((2 == '2') | ((2)== 2.0)) {
+//     console.log('cat2')
+// }
+
+
+// if ((4^2)<<(6|7)) {
+//     console.log('cat1')
+// }
+
+
+// var a=3, b=5
+// if (a&b | b|a) {
+//     console.log('cat2')
+// }
+
+
+
+// var a=5, b=7
+// if (a>=b & a<b) {
+//     console.log('cat2')
+// }
+
+
+
+// if ('Sahana' == 'Sahana'){
+//     console.log('cat1')          //true
+// }
+
+// if ('Sahana' == 'sahana'){
+//     console.log('cat2')         //false
+// }
+
+// if ('Sahana' === 'sahana'){
+//     console.log('cat3')         //false
+// }
+
+// if ('Sahana' === 'Sahana'){
+//     console.log('cat4')         //true
+// }
+
+
+
+
+
+
+
---
+++ conditions/conditions1.js
@@ -0,0 +1,86 @@
+// write a program to decide a given number is a positive/negative/zero
+
+// var a=-6;
+
+
+// if (a>0){
+//     console.log("its a positive number")
+// }
+
+// else if(a<0){
+//     console.log("its a negative number")
+// }
+
+// else{
+//     console.log("its a zero")
+// }
+
+
+
+
+
+
+
+// program to find even or odd number
+
+// var a = 3; 
+
+// if ((a%2)==0){
+//     console.log('its a even number')
+// }
+
+// else{
+//     console.log('its a odd number')
+// }
+
+
+
+
+
+// a program to ddecide which number is greater.
+
+
+// var a=2,b=-4;
+
+
+// if (a>b){
+//     console.log(`${a} a`)
+// }
+
+// else if (a<b){
+//     console.log(`${b} b`)
+// }
+
+// else{
+//     console.log('wrong input')
+// }
+
+
+
+
+
+
+// program to print sounds when in multiple of 3 and 5.
+
+
+var a=37;
+
+if (((a%3)==0) && ((a%5)==0)){
+    console.log('its multiple of both 3 and 5')
+}
+
+else if ((a%5)==0){
+    console.log('buzz')
+}
+
+else if ((a%3)==0){
+    console.log('fizz')
+}
+
+else{
+    console.log('not a multiple of 3 and 5')
+}
+
+
+
+// 
