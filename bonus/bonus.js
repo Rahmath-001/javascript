ff9cc1dec505a4206a2f1e49c5358952719daefe
Rahmath-001 <rehamathullah7@gmail.com>
2022-07-26 18:58:33 +0530

    bonus questions

---
+++ bonus/bonus.js
@@ -0,0 +1,50 @@
+// 1. Accept only numbers, Make a number always positive though given negative? With & without builtin function?
+
+// var a="3";
+
+// if (+a<0){
+//     console.log(Math.abs(a))
+// }
+// else if(+a>0) {
+//     console.log(a)
+// }
+// else {
+//     console.log('input is a string')
+// }
+
+
+
+
+
+
+
+// 2. A shop will give discount of 10% only if you purchase more than 1000 rupees.
+// Any item costs exactly 100 rupees. inputs = quantity of items.
+// Result must be Final bill prize.
+
+
+var a=1000
+
+
+a=((a-100)/a)*100;
+console.log(a)
+
+
+// if (a=100){
+//     console.log(a)
+// }
+// else if (a>=1000){
+//     console.log(a)
+// }
+// else{
+//     console.log(a)
+// }
+
+
+
+
+
+
+
+
+
