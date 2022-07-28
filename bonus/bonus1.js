5799f831ce2a54259adc6a2d56e279025bfe6da8
Rahmath-001 <rehamathullah7@gmail.com>
2022-07-28 20:27:59 +0530

    updated files of javascript

---
+++ bonus/bonus1.js
@@ -0,0 +1,153 @@
+
+//  program to find greatest of three numbers.
+
+// var a=4,b=14,c='hello'
+
+// if (+a==+b || +b==+c || +c==+a){
+    
+//     if (a==b && b==c && c==a){
+//         console.log('all are equal')
+//     }
+//     else if (a>b && a>c){
+//         console.log(`${a} a`)
+//     }
+//     else if (a<b && b>c){                  
+//         console.log(`${b} b`)
+//     }
+//     else if (c>a && c>b){
+//         console.log(`${c} c`)
+//     }
+//     else if (a==b){
+//     console.log(' a and b are equal ')
+//     }
+//     else if (b==c){
+//         console.log(' b and c are equal ')
+//     }
+//     else if (c==a){
+//         console.log(' a and c are equal ')
+//     }
+//     else {
+//         console.log('invalid 2 input')
+//     }
+// } 
+
+
+// if (a>b && a>c){
+//     console.log(`${a} a`)
+// }
+// else if (a<b && b>c){                  
+//     console.log(`${b} b`)
+// }
+// else if (c>a && c>b){
+//     console.log(`${c} c`)
+// }
+
+// else {
+//     console.log('invalid input')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CONFIRM WHETHER GIVEN NUMBER IS A 3 DIGIT NUMBER ?
+
+// var a=9996;
+// if ((a>99)) && (a<1000)){
+//     console.log(`the number is 3 digit`)
+// }
+// else{
+//     console.log('its not a 3 digit number')
+// }
+
+
+
+
+
+
+
+
+
+
+
+// CONFIRM WHETHER GIVEN NUMBER IS AN 'N' DIGIT NUMBER ?
+
+
+// var a=59588;
+// a=`${a}`
+// console.log(+a.length)
+
+
+
+
+
+
+
+
+
+
+
+// STUDENT > 75%
+// IF HE HAS MORE THAN 75, ALLOW IN EXAM
+// IF HE DOESNT HAS ASK FOR PERMISSION, IF YES ALLOW HIM
+
+
+// a=75;
+// if (a>=75) {
+//     console.log("enter the exam hall")
+// }
+// else if (a<75){
+//     console.log('ask permission')
+// }
+// else{
+//     console.log('not allowed in exam')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FIND WETHER THE GIVEN NUMBER IS PRIME OR NOT.
+
+// var n=99;
+// let check = 0;
+
+// if (n == 1){
+//     console.log("1 is neither prime nor composite")
+// }
+// else{
+// for (i=2;i<n;i++){
+
+//     if ((n%i) == 0 ){
+//         console.log("its not a prime number")
+//         console.log(n%i,i)
+//         check=1;
+//     } 
+
+// }
+//  if(check == 0) {
+//     console.log('its a prime number')
+// }
+// }
+
