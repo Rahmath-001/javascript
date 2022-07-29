9705149fe537c7242ed8f4c43c18da36516a3493
Rahmath-001 <rehamathullah7@gmail.com>
2022-07-29 15:16:01 +0530

    updated files of javascript practise

---
+++ bonus/bonus1.js
@@ -0,0 +1,89 @@
+// CONFIRM WHETHER GIVEN NUMBER IS A 3 DIGIT NUMBER ?
+
+// var a=9996;
+// if ((a<1000) && (a>99)){
+//     console.log(`the number is 3 digit`)
+// }
+// else{
+//     console.log('its not a 3 digit number')
+// }
+
+
+
+
+
+
+
+
+
+
+
+// CONFIRM WHETHER GIVEN NUMBER IS AN 'N' DIGIT NUMBER ?
+
+
+// var a=59588;
+// a=`${a}`
+// console.log(+a.length)
+
+
+
+
+
+
+
+
+
+
+
+
+// STUDENT > 75%
+// IF HE HAS MORE THAN 75, ALLOW IN EXAM
+// IF HE DOESNT HAS ASK FOR PERMISSION, IF YES ALLOW HIM
+
+
+// a=75;
+// if (a>=75) {
+//     console.log("enter the exam hall")
+// }
+// else if (a<75){
+//     console.log('ask permission')
+// }
+// else{
+//     console.log('not allowed in exam')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FIND WETHER THE GIVEN NUMBER IS PRIME OR NOT.
+
+// var n=99;
+// let check = 0;
+
+// if (n == 1){
+//     console.log("1 is neither prime nor composite")
+// }
+// else{
+// for (i=2;i<n;i++){
+
+//     if ((n%i) == 0 ){
+//         console.log("its not a prime number")
+//         console.log(n%i,i)
+//         check=1;
+//     } 
+
+// }
+//  if(check == 0) {
+//     console.log('its a prime number')
+// }
+// }
--- iteration/iteration1.js
+++ iteration/iteration1.js
@@ -92,6 +92,17 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
 // CONFIRM WHETHER GIVEN NUMBER IS A 3 DIGIT NUMBER ?
 
 // var a=9996;
@@ -107,14 +118,12 @@
 
 
 
-
-
 // CONFIRM WHETHER GIVEN NUMBER IS AN 'N' DIGIT NUMBER ?
 
-var a={n:5000}
 
-b=Object.values(a)
-console.log(b.length)
+// var a=59588;
+// a=`${a}`
+// console.log(+a.length)
 
 
 
@@ -122,11 +131,59 @@ console.log(b.length)
 
 
 // STUDENT > 75%
-// IF HE HAS MORE THAN 25, ALLOW IN EXAM
+// IF HE HAS MORE THAN 75, ALLOW IN EXAM
 // IF HE DOESNT HAS ASK FOR PERMISSION, IF YES ALLOW HIM
+
+
+// a=75;
+// if (a>=75) {
+//     console.log("enter the exam hall")
+// }
+// else if (a<75){
+//     console.log('ask permission')
+// }
+// else{
+//     console.log('not allowed in exam')
+// }
+
+
 
 
 
 
 
 // FIND WETHER THE GIVEN NUMBER IS PRIME OR NOT.
+
+// var n=99;
+// let check = 0;
+
+// if (n == 1){
+//     console.log("1 is neither prime nor composite")
+// }
+// else{
+// for (i=2;i<n;i++){
+
+//     if ((n%i) == 0 ){
+//         console.log("its not a prime number")
+//         console.log(n%i,i)
+//         check=1;
+//     } 
+
+// }
+//  if(check == 0) {
+//     console.log('its a prime number')
+// }
+// }
+
+
+// n % 1
+// n%n
+
+// var n=4;
+
+// if(((n%1)==0) && ((n%n)==0)){
+//     console.log('its a prime number')
+// }
+// else{
+//     console.log('its not a prime number')
+// }
