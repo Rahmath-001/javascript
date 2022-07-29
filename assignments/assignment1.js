0e9d4b41dbf4e4ca0dd5579e1b3c2f02924d9acb
Rahmath-001 <rehamathullah7@gmail.com>
2022-07-29 17:17:38 +0530

    updated files

---
+++ assignments/assignment1.js
@@ -0,0 +1,98 @@
+
+// CONFIRM WHETHER GIVEN NUMBER IS A 3 DIGIT NUMBER ?
+
+// var a=9996;
+// if ((a>99)) && (a<1000)){
+//     console.log(`the number is 3 digit`)
+// }
+// else{
+//     console.log('its not a 3 digit number')
+// }
+
+
+
+
+
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+// CONFIRM WHETHER GIVEN NUMBER IS AN 'N' DIGIT NUMBER ?
+
+
+// var a=59588;
+// a=`${a}`
+// console.log(+a.length)
+
+
+
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+// STUDENT > 75%
+// IF HE HAS MORE THAN 75, ALLOW IN EXAM
+// IF HE DOESNT HAS ASK FOR PERMISSION, IF YES ALLOW HIM
+
+
+
+
+
+// tc=70, ca=13;
+// p=(ca/tc)*100;
+
+// check=1;
+
+// if(p>75){
+//     console.log('allowed in exam')
+// }
+// else if( (p<75) && (check==0)){
+//     console.log('not allowed in exam')
+// }
+// else{
+//     console.log('allowed in exam')
+// }
+
+
+
+
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+// FIND WETHER THE GIVEN NUMBER IS PRIME OR NOT.
+
+// var n=99;
+// let check = 0;
+
+// if (n == 1){
+//     console.log("1 is neither prime nor composite")
+// }
+// else{
+// for (i=2;i<n;i++){
+
+//     if ((n%i) == 0 ){
+//         console.log("its not a prime number")
+//         console.log(n%i,i)
+//         check=1;
+//     } 
+
+// }
+//  if(check == 0) {
+//     console.log('its a prime number')
+// }
+// }
+
+
