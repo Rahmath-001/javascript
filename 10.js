f798b7304b22b4c834d16e5f7c068ed286e0e9c3
Rahmath-001 <rehamathullah7@gmail.com>
2022-08-10 10:58:00 +0530

    updated files of javascript practise

---
+++ 10.js
@@ -0,0 +1,112 @@
+// WAP to multipy two numbers without using astrics.
+
+// a/2 if even numbers terminate the line.
+// b*2  and add the numbers of remaing lines 
+
+// c=a/2, if c==c%2 && c==1 return
+// d=b*2,  e=e+d
+
+
+
+// c=0;
+// d=0;
+// e=0;
+// a=19 ;b=27 ;
+
+// for(i=0;i<=a;i++){
+    // c=Math.floor(a/2);
+    // d=b*2;
+    // e=e+d;
+    // if( (c>1) && (0 !==(c%2))){
+        // d=b*2;
+        // e=e+d;
+        // for(c=a;c>1;){
+        // c=Math.floor(a/2);
+        // d=b*2;
+        // e=e+d;
+        // c--
+        // c=Math.floor(a/2);
+        // console.log(a,b)
+        // console.log(c,d,e)
+        // c--
+        // return 'line cut';
+    // }
+// }
+
+
+
+
+    // console.log(`${c} ${d}  ${e}`)
+    // console.log(e)
+// }
+    // else{
+    //     for(let j=i;j<=a;j++){
+    //     d=b*2;
+    //     e=e+d;
+    //     }
+    // console.log
+// }
+
+
+
+
+
+
+
+
+
+
+//   M  A  T  R  I  C   X
+
+
+
+// FOR ADDITION I x J == I x J
+
+// FOR SUBSTRACTION I x J == I x J
+
+// FOR MULTIPLICATION IF AxB CxD  CONDITION  ( B==C )   AND OUTPUT (AxD)
+
+
+
+
+
+
+
+//  WAP to add two matrix
+
+a=[123]
+b=[123]
+
+// for(row=0;row<=a.length;row++){
+    for(column=0;column<=a.length-1;column++){
+        console.log(a)
+    }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// assignment           //  substract two numbers without using - sign. 
+
+// BONUS     // FIND THE INDEX VALUE OF A 2D ARRAY.
+
+
+
+
+
+
+
