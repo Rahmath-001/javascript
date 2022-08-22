9686038025bab5762b3751efc8d53347ba119937
Rahmath-001 <rehamathullah7@gmail.com>
2022-08-22 14:08:48 +0530

    DECODER

--- base64/decoder.js
+++ base64/decoder.js
@@ -24,20 +24,63 @@
 
 let finalbase64string = "SEVMTE8=";
 let base64="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-if (finalbase64string == ( "==" || "=")){
-    finalbase64string.pop()
-}
-
-else {
-     finalbase64string.map((num) =>base64.indexOf(num))
-}
-
 console.log(finalbase64string)
+
+ 
+let dec=finalbase64string.split("")
 console.log(dec)
-console.log(final)
 
 
+// let lastelement=binarycode6bit[binarycode6bit.length-1]
+var counter=0;
+if(dec[dec.length-1] == "="){
+    dec.pop()
+    counter++;
+    if(dec[dec.length-1] == "="){
+    dec.pop()
+    counter++;
+    }
+}
+
+let decimal =  dec.map((num) =>base64.indexOf(num))
+console.log(decimal)
 
 
+let binary = decimal.map((bin) =>bin.toString(2))   // decimal.toString(2)
+console.log(binary)
 
+let binarycode6bit=binary.map((bin)=>{
+    while(bin.length<6){
+       bin="0"+bin;
+    }
+    return bin;
+    })
+   console.log(binarycode6bit) 
+
+// let binary8bit = binarycode6bit.join("")
+
+// console.log(binary8bit)
+
+let lastelement = binarycode6bit[binarycode6bit.length-1]
+while(counter>0){
+//   lastelement = lastelement.split("")
+  lastelement =lastelement.slice(0,lastelement.length-2)
+  counter--;
+}
+binarycode6bit.pop()
+binarycode6bit.push(lastelement)
+
+binarycode6bit=binarycode6bit.join("").split("");
+
+console.log(binarycode6bit)
+let binarycodejoin=[];
+    while(binarycode6bit.length !=0){
+        binarycodejoin.push(binarycode6bit.splice(0,8).join(""))
+    }
+console.log(binarycodejoin)
+
+let bintodec = binarycodejoin.map((char)=>parseInt(char,2));
+console.log(bintodec)
+
+let decode=bintodec.map((temp) => String.fromCharCode(temp))
+console.log(decode)
