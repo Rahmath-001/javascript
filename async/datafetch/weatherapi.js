fd4a0a6cc03b10f871e20ac655f512fa57fdda00
Rahmath-001 <rehamathullah7@gmail.com>
2022-09-06 20:26:02 +0530

    WEATHER CLI COMPLETED

--- async/datafetch/weatherapi.js
+++ async/datafetch/weatherapi.js
@@ -1,26 +1,6 @@
 import axios from "axios";
 import readlineSync from "readline-sync";
 
-// function getWeather() {
-//     try {
-//         let cityname = readlineSync.question("Enter Cityname : ");
-//         while (!cityname) {
-//             cityname = readlineSync.question("City name cant be Empty please Re-enter the Name : ");
-//         }
-
-//         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=a06f7a0d0fae7926aa6e13224d71923f`)
-//             .then((res) => {
-//                 console.log(res.data);
-//             })
-//             .catch((err) => {
-//                 // console.error(err);
-//                 console.error(err.response.data);
-//             })
-
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
 
 
 async function getWeather() {
@@ -30,8 +10,8 @@ async function getWeather() {
             cityname = readlineSync.question("City name cant be Empty please Re-enter the Name : ");
         }
 
-
-        let res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=a06f7a0d0fae7926aa6e13224d71923f`)
+        const appid = 'ffdd323148e1c6a474a68dad2741d561';
+        let res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${appid}`)
 
 
     console.log(`\n CHOICES :  \n 0) EXIT \n 1) MAX-Temperature\n 2) MIN-Temperature\n 3) HUMIDITY\n 4) SUNSET\n 5) SUNRISE\n `)
@@ -90,19 +70,6 @@ async function getWeather() {
            console.log("\nTHANK YOU for using our Application......\n")
         }
 
-
-        // console.log(res.data);
-
-        //The Temperature in X city is _ Centrigrade
-        // console.log(`\n -----------------------------------------`)
-        // console.log(`  The Temperature in ${cityname} is ${(res.data.main.temp - 273).toFixed(2)} Centigrade`)
-        // console.log(`  The MAX-Temperature in ${cityname} is ${(res.data.main.temp_max - 273).toFixed(2)} Centigrade`)
-        // console.log(`  The MIN-Temperature in ${cityname} is ${(res.data.main.temp_min - 273).toFixed(2)} Centigrade`)
-        // console.log(`  The HUMIDITY in ${cityname} is ${(res.data.main.humidity).toFixed(2)} Centigrade`)
-        // console.log(`  The sunset in ${cityname} is ${(res.data.sys.sunset)} Centigrade`)
-        // console.log(`  The sunrise in ${cityname} is ${(res.data.sys.sunrise)} Centigrade`)
-
-        // console.log(` -----------------------------------------\n`)
 
     } catch (error) {
         console.error(error);
