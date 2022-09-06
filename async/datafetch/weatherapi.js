7f0271d3ac2954916ff23d01b94d5869d49ebceb
Rahmath-001 <rehamathullah7@gmail.com>
2022-09-06 20:02:50 +0530

    WEATHER CLI

--- async/datafetch/weatherapi.js
+++ async/datafetch/weatherapi.js
@@ -25,27 +25,79 @@ import readlineSync from "readline-sync";
 
 async function getWeather() {
     try {
-        let cityname = readlineSync.question("Enter Cityname : ");
+        let cityname = readlineSync.question("\nEnter Cityname : \n");
         while (!cityname) {
             cityname = readlineSync.question("City name cant be Empty please Re-enter the Name : ");
         }
 
 
-        switch (cityname){
-            case 0: 
-        }
-
         let res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=a06f7a0d0fae7926aa6e13224d71923f`)
+
+
+    console.log(`\n CHOICES :  \n 0) EXIT \n 1) MAX-Temperature\n 2) MIN-Temperature\n 3) HUMIDITY\n 4) SUNSET\n 5) SUNRISE\n 6) REPEAT \n\n`)
+
+        
+        let choice=readlineSync.questionInt("Enter choice to get details : ")
+        switch (choice){
+            case 0: 
+            console.log(`\n -----------------------------------------`)
+            console.log(" THNAK YOU FOR USING WEATHER API ")
+            console.log(` -----------------------------------------\n`)
+            procress.exit();
+            break;
+
+            case 1: 
+            console.log(`\n -----------------------------------------`)
+            console.log(`  The MAX-Temperature in ${cityname} is ${(res.data.main.temp_max - 273).toFixed(2)} Centigrade`);
+            console.log(` -----------------------------------------\n`)
+            break;
+
+            case 2: 
+            console.log(`\n -----------------------------------------`)
+            console.log(`   The MIN-Temperature in ${cityname} is ${(res.data.main.temp_min - 273).toFixed(2)} Centigrade`);
+            console.log(` -----------------------------------------\n`)
+            break;
+
+
+            case 3:
+                console.log(`\n -----------------------------------------`)
+                console.log(`  The HUMIDITY in ${cityname} is ${(res.data.main.humidity).toFixed(2)} Centigrade`);
+                console.log(` -----------------------------------------\n`)
+            break;    
+
+            case 4:
+                console.log(`\n -----------------------------------------`)
+                console.log(`  The SUNSET in ${cityname} is ${(res.data.sys.sunset)} Centigrade`);
+                console.log(` -----------------------------------------\n`)
+            break;
+            
+            case 5:
+                console.log(`\n -----------------------------------------`)
+                console.log(`  The SUNRISE in ${cityname} is ${(res.data.sys.sunrise)} Centigrade`);
+                console.log(` -----------------------------------------\n`)
+            break;
+
+            default: 
+            console.log("Invalid input ")
+        }
 
 
         // console.log(res.data);
 
         //The Temperature in X city is _ Centrigrade
+        // console.log(`\n -----------------------------------------`)
+        // console.log(`  The Temperature in ${cityname} is ${(res.data.main.temp - 273).toFixed(2)} Centigrade`)
+        // console.log(`  The MAX-Temperature in ${cityname} is ${(res.data.main.temp_max - 273).toFixed(2)} Centigrade`)
+        // console.log(`  The MIN-Temperature in ${cityname} is ${(res.data.main.temp_min - 273).toFixed(2)} Centigrade`)
+        // console.log(`  The HUMIDITY in ${cityname} is ${(res.data.main.humidity).toFixed(2)} Centigrade`)
+        // console.log(`  The sunset in ${cityname} is ${(res.data.sys.sunset)} Centigrade`)
+        // console.log(`  The sunrise in ${cityname} is ${(res.data.sys.sunrise)} Centigrade`)
 
-        console.log(`The Temperature in ${cityname} is ${(res.data.main.temp - 273).toFixed(2)} Centigrade`)
+        // console.log(` -----------------------------------------\n`)
+
     } catch (error) {
         console.error(error);
-        console.error(err.response.data);
+        console.error(error.response.data);
     }
 }
 
