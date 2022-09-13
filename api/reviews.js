ee9d351d79f8ffeaf7672bda240552cc8ea55c42
Rahmath-001 <rehamathullah7@gmail.com>
2022-09-13 19:53:04 +0530

    resturant API creation

---
+++ api/reviews.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import readlineSync from "readline-sync";
+
+
+
+
+
+
+// async function resturantdata() {
+//     try {
+
+        let resturantlink = await axios.get(`https://gist.githubusercontent.com/Jkudjo/8f64c6fe24f30e754fa3ce4971063826/raw/cbd57cfdddbdfc009fd9ccdadf5fb7129af71c73/restaurant-data.json`)
+
+        console.log(resturantlink.restaurants.operating_hours)
+
+
+//     } catch (error) {
+//         console.error(error);
+//         console.error(error.response);
+//     }
+// }
+
+resturantdata();
